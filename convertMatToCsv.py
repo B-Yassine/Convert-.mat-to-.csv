4194c29c5808016c82bf8127b6d2073a5faddd0b
BAKHTI Yassine <ybakhti@ito.dz>
2019-05-28 18:57:08 +0200

    Update convertMatToCsv.py

--- convertMatToCsv.py
+++ convertMatToCsv.py
@@ -1,9 +1,9 @@
 import scipy.io
 import numpy as np
 
-
-data = scipy.io.loadmat("name_ordre.mat")
+data = scipy.io.loadmat("yourFile.mat")
 
 for i in data:
     if '__' not in i and 'readme' not in i:
-        np.savetxt(("name_ordre.csv"), data[i], fmt='%s',delimiter=',')
+        np.savetxt(("yourFile.csv"), data[i], delimiter=',')
+ 
